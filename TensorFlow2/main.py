e84f529a66342c8d5a51e5c7d88d08280593186b
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-05-14 09:02:44 -0400

    init

---
+++ TensorFlow2/main.py
@@ -0,0 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+
+# Initialization of Tensors
+x = tf.constant(4)
+print(x)
+
+# Mathematical Operations
+
+# Indexing
+
+# Reshaping
