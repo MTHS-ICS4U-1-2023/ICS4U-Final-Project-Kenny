67d98c67a2d8202728cecf8e8776676bc8e3a4d3
EC2 Default User <ec2-user@ip-172-31-30-96.ec2.internal>
2024-05-30 15:20:21 +0000

    TF6

--- TensorFlow5/main.py
+++ TensorFlow5/main.py
@@ -11,49 +11,55 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers, regularizers
-from tensorflow.keras.datasets import cifar10
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train = x_train.astype("float32") / 255.0
-x_test = x_test.astype("float32") / 255.0
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
+# x_test = x_test.reshape(-1, 784).astype("float32") / 255.0
+x_train = x_train.reshape([-1, 28, 28]).astype("float32") / 255.0
+x_test = x_test.reshape([-1, 28, 28]).astype("float32") / 255.0
 
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(layers.SimpleRNN(512, return_sequences=True, activation="relu"))
+model.add(layers.SimpleRNN(512, activation="relu"))
+model.add(layers.Dense(10))
 
-def my_model():
-    inputs = keras.Input(shape=(32, 32, 3))
-    x = layers.Conv2D(32, 3, padding="same", kernel_regularizer=regularizers.l2(0.01),)(
-        inputs
-    )
-    x = layers.BatchNormalization()(x)
-    x = keras.activations.relu(x)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(64, 3, padding="same", kernel_regularizer=regularizers.l2(0.01),)(
-        x
-    )
-    x = layers.BatchNormalization()(x)
-    x = keras.activations.relu(x)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(
-        128, 3, padding="same", kernel_regularizer=regularizers.l2(0.01),
-    )(x)
-    x = layers.BatchNormalization()(x)
-    x = keras.activations.relu(x)
-    x = layers.Flatten()(x)
-    x = layers.Dense(64, activation="relu", kernel_regularizer=regularizers.l2(0.01),)(
-        x
-    )
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(10)(x)
-    model = keras.Model(inputs=inputs, outputs=outputs)
-    return model
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(layers.SimpleRNN(256, return_sequences=True, activation="tanh"))
+model.add(layers.SimpleRNN(256))
+model.add(layers.Dense(10))
 
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(layers.GRU(256, return_sequences=True, activation="relu"))
+model.add(layers.GRU(256))
+model.add(layers.Dense(10))
 
-model = my_model()
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(
+    layers.Bidirectional(layers.LSTM(256, return_sequences=True, activation="relu"))
+)
+model.add(layers.LSTM(256, name="lstm_layer2"))
+model.add(layers.Dense(10))
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(None, 28)))
+model.add(
+    layers.Bidirectional(layers.LSTM(256, return_sequences=True, activation="relu"))
+)
+model.add(layers.Bidirectional(layers.LSTM(256, name="lstm_layer2")))
+model.add(layers.Dense(10))
+
+print(model.summary())
 model.compile(
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
     metrics=["accuracy"],
 )
 
-model.fit(x_train, y_train, batch_size=64, epochs=150, verbose=2)
+model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
 model.evaluate(x_test, y_test, batch_size=64, verbose=2)
